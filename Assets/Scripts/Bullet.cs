8d5c03b213fcb657d4b8903b559fb177a74c5512
UVictoras <vmartinan@gaming.tech>
2024-04-06 12:13:12 +0200

    LifeTime of bullets

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,15 +6,38 @@ public class Bullet : MonoBehaviour
 {
     public float speed;
     private Rigidbody2D rigidBody;
+    private int _lifeTime;
+    private bool _canCycle;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
         rigidBody.velocity = transform.right * speed;
+        _lifeTime = 5;
+        _canCycle = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_lifeTime == 0)
+        {
+            Destroy(gameObject);
+        }
+        if (_canCycle == false)
+            return;
+
+        StartCoroutine(CycleLife());
+    }
+
+    IEnumerator CycleLife()
+    {
+        _canCycle = false;
+
+        yield return new WaitForSeconds(1);
+
+        _lifeTime--;
+
+        _canCycle = true;
     }
 }
