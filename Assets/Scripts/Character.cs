50bb990effced28bff632472d4078d205f013af8
puumbaaa <78651716+puumbaaa@users.noreply.github.com>
2024-04-11 15:30:50 +0200

    ça arrive

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -27,6 +27,8 @@ public class Character : MonoBehaviour
     public bool _isGrounded;
     [HideInInspector]
     public TextMeshProUGUI _percentageText;
+    [HideInInspector]
+    public string _direction;
 
     public int _jumpsLeft;
     public int _lifesRemaining;
@@ -48,6 +50,7 @@ public class Character : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        _direction = "right";
         _body = GetComponent<Rigidbody2D>();
         _maxJumps = 2;
         _jumpsLeft = _maxJumps;
@@ -60,6 +63,7 @@ public class Character : MonoBehaviour
             int offset = _playerId == 1 ? 0 : 3;
             _hearts.Add(Main.instance._hearts[i + offset]);
         }
+
     }
 
     // Update is called once per frame
@@ -77,7 +81,11 @@ public class Character : MonoBehaviour
         float horizontalInput = _playerId == 1 ? Input.GetAxis("Horizontal") : Input.GetAxis("Horizontal2");
         Vector3 movement = new Vector3(horizontalInput * _speed * Time.deltaTime, 0.0f, 0.0f);
         transform.position += movement;
-        transform.rotation = Quaternion.Euler(0, horizontalInput < 0 ? 180 : 0, 0);
+        if (horizontalInput < 0 )
+            _direction = "left";
+        else if (horizontalInput > 0 ) 
+            _direction = "right";
+        transform.rotation = Quaternion.Euler(0, _direction == "left"? 180 : 0, 0);
     }
 
     private void Jump()
@@ -125,23 +133,36 @@ public class Character : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "attack" || collision.tag == "Axe")
+        
+        if (collision.tag == "attack" || collision.tag == "Axe" || collision.tag == "Player" || collision.tag == "specialAttack")
         {
+            print("aaa");
             GameObject attack = collision.gameObject;
             if (attack.tag == "Axe")
             {
                 //_animator.SetTrigger("TakeDamage");
-                Vector2 pushBack = new Vector2(_percentage * (1 / attack.GetComponent<Bullet>()._strenght) * attack.transform.localPosition.x, _percentage * (1 / attack.GetComponent<Bullet>()._strenght) * attack.transform.localPosition.y);
+                Vector2 pushBack = new Vector2((_percentage * attack.GetComponent<Bullet>()._strenght * attack.transform.localPosition.x) * 2, (_percentage * 1 / attack.GetComponent<Bullet>()._strenght * attack.transform.localPosition.y) * 2);
                 _percentage += attack.GetComponent<Bullet>()._damage;
                 _body.AddForce(pushBack);
             }
             else if (attack.tag == "attack")
             {
-                print("papagnan");
                 //_animator.SetTrigger("TakeDamage");
-                Vector2 pushBack = new Vector2((_percentage *  attack.GetComponent<PlayerBaseAttack>()._strenght * attack.transform.localPosition.x) * 5, (_percentage * attack.GetComponent<PlayerBaseAttack>()._strenght * attack.transform.localPosition.y) * 5);
+                Vector2 pushBack = new Vector2((_percentage *  attack.GetComponent<PlayerBaseAttack>()._strenght * attack.transform.localPosition.x) * 2, (_percentage * attack.GetComponent<PlayerBaseAttack>()._strenght * attack.transform.localPosition.y) * 2);
                 _percentage += attack.GetComponent<PlayerBaseAttack>()._damage;
                 _body.AddForce(pushBack);
+            }
+            else if (attack.tag == "Player") 
+            {
+                if (attack.GetComponent<SpecialAttackOdin>() != null)
+                {
+                    if (attack.GetComponent<SpecialAttackOdin>()._isActive)
+                    {
+                        Vector2 pushBack = new Vector2((_percentage * attack.GetComponent<SpecialAttackOdin>()._strenght * attack.transform.localPosition.x) * 2, (_percentage * attack.GetComponent<SpecialAttackOdin>()._strenght * attack.transform.localPosition.y) * 2);
+                        _percentage += attack.GetComponent<SpecialAttackOdin>()._damage;
+                        _body.AddForce(pushBack);
+                    }
+                }
             }
             
         }
